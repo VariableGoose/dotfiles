d9b2f83f546d5750ac1d73cd445f70c8bff6d433
Linus Kåreblom <linus.kareblom@gmail.com>
2025-01-23 17:18:38 +0100

    F2 continues debug instead of rename and autocmd for LaTeX files.

--- general/.config/nvim/lua/linusepk/autocmd.lua
+++ general/.config/nvim/lua/linusepk/autocmd.lua
@@ -5,3 +5,11 @@ vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
         vim.opt.linebreak = true;
     end,
 });
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
+    pattern = "*.tex",
+    callback = function ()
+        vim.opt.textwidth = 80;
+        vim.opt.spell = true;
+    end,
+});
